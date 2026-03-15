299c42273d3cb923ef0629c7456b8c239e0a2034
Yuliia Shefner <yul.she91@gmail.com>
2026-03-15 18:13:46 +0100

    done all tasks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import type { Movie } from "../../types/movie";
 import toast, { Toaster } from "react-hot-toast";
@@ -8,28 +8,31 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import { fetchMovies } from "../../services/movieService";
 import MovieModal from "../MovieModal/MovieModal";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
 
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-  const handleSearch = async (query: string) => {
-    try {
-      setMovies([]);
-      setIsLoading(true);
-      setIsError(false);
-      const data = await fetchMovies(query);
-      if (data.length === 0) {
-        toast.error("No movies found for your request", { duration: 2000 });
-      }
-      setMovies(data);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["movie", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
+  });
+
+  const handleSearch = (topic: string) => {
+    setQuery(topic);
+    setPage(1);
   };
+
+  useEffect(() => {
+    if (isSuccess && data?.results.length === 0) {
+      toast.error("No movies found for your request", { duration: 2000 });
+    }
+  }, [data, isSuccess]);
 
   const handleMovieSelect = (movie: Movie) => {
     setSelectedMovie(movie);
@@ -45,8 +48,23 @@ function App() {
       <SearchBar onSubmit={handleSearch} />
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
-      {movies.length > 0 && (
-        <MovieGrid movies={movies} onSelect={handleMovieSelect} />
+      {isSuccess && (
+        <>
+          {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+          <MovieGrid movies={data.results} onSelect={handleMovieSelect} />
+        </>
       )}
       {selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={handleModalClose} />
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,13 +2,15 @@ import axios from "axios";
 import type { Movie } from "../types/movie";
 
 interface MoviesHttpResponse {
-  page: number;
   results: Movie[];
   total_pages: number;
-  total_results: number;
+  page: number;
 }
 
-export const fetchMovies = async (topic: string): Promise<Movie[]> => {
+export const fetchMovies = async (
+  topic: string,
+  page: number,
+): Promise<MoviesHttpResponse> => {
   const myKey = import.meta.env.VITE_TMDB_TOKEN;
   const response = await axios.get<MoviesHttpResponse>(
     "https://api.themoviedb.org/3/search/movie",
@@ -17,7 +19,7 @@ export const fetchMovies = async (topic: string): Promise<Movie[]> => {
         query: topic,
         include_adult: false,
         language: "en-US",
-        page: 1,
+        page,
       },
       headers: {
         accept: "application/json",
@@ -25,5 +27,5 @@ export const fetchMovies = async (topic: string): Promise<Movie[]> => {
       },
     },
   );
-  return response.data.results;
+  return response.data;
 };
